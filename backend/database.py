7af2275ad16dbdebf4928e1384eea8a1198d8402
JoseVColino <josevictorcolino@gmail.com>
2025-10-18 19:23:44 -0300

    doing the thing right

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)  # important
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
